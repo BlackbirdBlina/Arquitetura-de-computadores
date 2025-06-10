a4d067c5e7ecf7cf56abe0184ea53c3209e992b8
Sabrina Barbosa <sabrinapop_rn@hotmail.com>
2025-06-10 14:31:10 -0300

    Alterando instrução bltz (pseudocódigo) substituindo pelo uso das instruções slt e bne, mantendo o mesmo funcionamento anterior. (Incompleto)

--- Unidade 2/ordenaVetor.asm
+++ Unidade 2/ordenaVetor.asm
@@ -9,23 +9,25 @@ main:
 	la $t1, vetor		#la - load address - ler o endereço de memória do vetor e salva em t1
 
 
-	lw $t2, 0($t1)		#lw - load word - Carrega em t2 o que está armazenado no endereço de memória informado por t1 + 0
-	lw $t3, 4($t1)		#lw - load word - Carrega em t3 o que está armazenado no endereço de memória informado por t1 + 4
-	sub $t4, $t3, $t2	#sub - Subtrai os valores passados e armazena o resultado em t4
-	bltz $t4, success	#bgez - branch if less than zero - Verifica se o resultado é menor do que 0
+	lw $t2, 0($t1)		#lw - load word - Carrega em $t2 o que está armazenado no endereço de memória informado por $t1 + 0
+	lw $t3, 4($t1)		#lw - load word - Carrega em $t3 o que está armazenado no endereço de memória informado por $t1 + 4
+	sub $t4, $t3, $t2	#sub - Subtrai os valores passados e armazena o resultado em $t4
+	slt $t5, $t4, 0		#slt - Set on less Than - Verifica se $t4 é menor do que 0 e carrega em $t5 o resultado da comparação. Se menor recebe 1, se não -> 0
+	bne $t5, 0, success	#beq - Branch if Not Equal - Verifica se $t4 é diferente de 0 e "pula" para a a label success
 	j failure
 
 #Acontece troca
 success:
-	sw $t3, 0($t1)		#sw - store word - Coloca o valor armazenado em $t3 na primeira posição do vetor, através do endereço de memória apontado por t1 + 0
-	sw $t2, 4($t1)		#sw - store word - Coloca o valor armazenado em $t2 na segunda posição do vetor, através do endereço de memória apontado por t1 + 4
+	sw $t3, 0($t1)		#sw - store word - Coloca o valor armazenado em $t3 na primeira posição do vetor, através do endereço de memória apontado por $t1 + 0
+	sw $t2, 4($t1)		#sw - store word - Coloca o valor armazenado em $t2 na segunda posição do vetor, através do endereço de memória apontado por $t1 + 4
 
 #Não acontece troca e avança para a próxima linha
 failure:
-	lw $t2, 4($t1)		#lw - load word - Carrega em t5 o que está armazenado no endereço de memória informado por t1 + 4
-	lw $t3, 8($t1)		#lw - load word - Carrega em t6 o que está armazenado no endereço de memória informado por t1 + 8 (o que está anterior atualizado)
-	sub $t4, $t3, $t2	#sub - Subtrai os valores passados e armazena o resultado em t7
-	bltz $t4, success2	#bgez - branch if less than zero - Verifica se o resultado é menor do que 0
+	lw $t2, 4($t1)		#lw - load word - Carrega em $t2 o que está armazenado no endereço de memória informado por t1 + 4
+	lw $t3, 8($t1)		#lw - load word - Carrega em $t3 o que está armazenado no endereço de memória informado por $t1 + 8 (o que está anterior atualizado)
+	sub $t4, $t3, $t2	#sub - Subtrai os valores passados e armazena o resultado em $t4
+	slt $t5, $t4, 0		#slt - Set on less Than - Verifica se $t4 é menor do que 0 e carrega em $t5 o resultado da comparação. Se menor recebe 1, se não -> 0
+	bne $t5, 0, success	#beq - Branch if Not Equal - Verifica se $t4 é diferente de 0 e "pula" para a a label success
 	j failure2
 
 #Acontece troca
